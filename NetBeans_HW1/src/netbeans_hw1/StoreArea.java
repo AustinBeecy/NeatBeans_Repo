37952ce51692a093ad2cc4a19e829c8c1b1e1300
ZTaylor2022 <106450964+ZTaylor2022@users.noreply.github.com>
2022-05-28 18:10:41 -0400

    branch test

---
+++ NetBeans_HW1/src/netbeans_hw1/StoreArea.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeans_hw1;
+
+/**
+ *
+ * @author black
+ */
+public class StoreArea {
+    String areaID;
+    String name;
+    String description;
+    String state;
+}
