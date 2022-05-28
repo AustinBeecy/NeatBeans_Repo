716a57c91e56043ab9bb6b234644c72e556145e8
ZTaylor2022 <106450964+ZTaylor2022@users.noreply.github.com>
2022-05-28 17:59:48 -0400

    hello austin

--- NetBeans_HW1/src/netbeans_hw1/NetBeans_HW1.java
+++ NetBeans_HW1/src/netbeans_hw1/NetBeans_HW1.java
@@ -14,7 +14,7 @@ public class NetBeans_HW1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("It Worked!");
+        System.out.print("Hello Austin!");
     }
     
 }
