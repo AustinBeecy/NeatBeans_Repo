d819245adfdd9c9a529163bd9e497ee8e50cabbb
maxknight50 <maxknight50@gmail.com>
2022-05-30 12:47:58 -0400

    Commit Test

--- NetBeans_HW1/src/netbeans_hw1/NetBeans_HW1.java
+++ NetBeans_HW1/src/netbeans_hw1/NetBeans_HW1.java
@@ -1,7 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
+// Commit Test
+
 package netbeans_hw1;
 
 /**
