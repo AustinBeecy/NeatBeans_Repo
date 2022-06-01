ae766b19485d60d05bed298ef4848e675cc8f7f7
Austin Beecy <Jd3abc@gmail.com>
2022-06-01 17:11:16 -0400

    Started FXML and Controller Set up

--- NetBeans_HW1/src/grocery/SwitchScene1.java
+++ NetBeans_HW1/src/grocery/SwitchScene1.java
@@ -1,5 +1,6 @@
 package grocery;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -7,6 +8,7 @@ import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -24,95 +26,18 @@ import oracle.jdbc.pool.OracleDataSource;
 // then populate a data entry form with that info, allowing them to only change the 1 or 2 fields they want to edit, then save.
 
 public class SwitchScene1 extends Application {
-    Statement stmt;
-    static Connection conn;
-    static String currentStore = null; 
-    Stage window;
-    Scene scene1, scene2, stores, storeOptions; 
-    
-   
-        
+    Stage window = new Stage();
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        conn = conOracle("javauser", "javapass");
-        stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-       // ResultSet employee = stmt.executeQuery("select * from Employee");
-        ResultSet store = stmt.executeQuery("select * from Store");
+    public void start(Stage primaryStage) throws IOException {
         
-
-        window = primaryStage;
-
-        // Store Scene 
-        // Run SQL query to get store locations
-        // generate lables from SQL Query with a for loop 
-        ArrayList<Label> storeLabels = new ArrayList<Label>();
-        
-//        storeLabels.add(new Label("Virginia")); // will be replaced with sql results
-//        storeLabels.add(new Label("Tennessee")); // will be replaced with sql results
-
-        // same replace buttons with sql results in for loop 
-        // ** Probably won't work with different data, ex multiple cities in VA all under VA state. 
-        ArrayList<Button> storeButtons = new ArrayList<Button>();
-        while (store.next()) {
-            storeLabels.add(new Label("Available stores in " + store.getString("State")));
-            storeButtons.add(new Button(store.getString("City")));
-        }
-        
-       // create layout for stores
-        VBox storeLayout = new VBox(20);
-        storeLayout.setPadding(new Insets(20));
-        
-        //populate stores layout with labels and buttons 
-        for(int i = 0; i < storeLabels.size(); i++){
-            storeLayout.getChildren().add(storeLabels.get(i));
-            // for loop for buttons 
-            storeLayout.getChildren().add(storeButtons.get(i));
-            storeButtons.get(i).setOnAction(e -> {
-              Button b = (Button) e.getSource(); 
-               pickStore(b.getText()); 
-                System.out.println(currentStore);
-                window.setScene(storeOptions);
-            });
-        }
-       
-        // create stores scene 
-        stores = new Scene(storeLayout, 500, 400);
-        
-        //Create Label for store option
-        Label welcomeLabel = new Label("Welcome!"); 
-        // create buttons for store options 
-        ArrayList<Button> storeOptionsButtons = new ArrayList<Button>(); 
-        storeOptionsButtons.add(new Button("Employees"));
-        storeOptionsButtons.add(new Button("Products"));
-        storeOptionsButtons.add(new Button("Store Areas"));
-        storeOptionsButtons.add(new Button("Suppliers"));
-        storeOptionsButtons.add(new Button("Inventory"));
-        
-        //create layout for store options 
-        VBox storeOptionsLayout = new VBox(20);
-        storeOptionsLayout.setPadding(new Insets(20));
-        
-        storeOptionsLayout.getChildren().add(welcomeLabel);
-        for(Button storeOptionsButton: storeOptionsButtons){
-            storeOptionsLayout.getChildren().add(storeOptionsButton);
-            storeOptionsButton.setOnAction(e -> window.setScene(stores));
-        }
-        //create scene 
-        storeOptions = new Scene(storeOptionsLayout, 500, 400);
-//        Parent homeRoot = FXMLLoader.load(getClass().getResource("resources/storePick.fxml"));
-//        Scene test = new Scene(homeRoot);
-        window.setScene(stores);
+        Parent root = FXMLLoader.load(getClass().getResource("resources/storePick.fxml"));
+         Scene scene = new Scene(root);
+         window.setScene(scene);
         window.setTitle("Store Selection");
         window.show();
     }
-     static Connection conOracle(String id, String pw) throws Exception {
-        String connectionString = "jdbc:oracle:thin:@localhost:1521:XE";
-        OracleDataSource ds = new OracleDataSource();   
-        ds.setURL(connectionString);
-        return ds.getConnection(id, pw);
-    }
-    void pickStore(String button){
-        currentStore = button; 
+    public Stage getWindow() throws IOException{
+         return this.window; 
     }
     public static void main(String[] args) {
         launch(args);
--- NetBeans_HW1/src/grocery/resources/StorePickController.java
+++ NetBeans_HW1/src/grocery/resources/StorePickController.java
@@ -4,9 +4,29 @@
  */
 package grocery.resources;
 
+import grocery.SwitchScene1;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import oracle.jdbc.pool.OracleDataSource;
 
 /**
  * FXML Controller class
@@ -15,12 +35,46 @@ import javafx.fxml.Initializable;
  */
 public class StorePickController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    public GridPane gridPane; 
+    Statement stmt;
+    static Connection conn;
+    static String currentStore = ""; 
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+         try {
+             // TODO
+             conn = conOracle("javauser", "javapass");
+             stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+             ResultSet store = stmt.executeQuery("select * from Store");
+             int i = 1; 
+             String welcomeFXML = "resources/welcome.fxml";
+             while(store.next()){
+             Text text = new Text(store.getString("State"));
+             // add styling to text
+             Button button = new Button(store.getString("City"));
+             // add Styling to button
+             SwitchScene1 ss = new SwitchScene1(); 
+             button.setOnAction(e -> {
+                System.out.println("Need to find out how to changes scenes via controllers");
+             });
+               
+  
+             gridPane.add(text, 1, i);
+             i++;
+             gridPane.add(button, 1, i);
+             i++; 
+             }
+         } catch (Exception ex) {
+             Logger.getLogger(StorePickController.class.getName()).log(Level.SEVERE, null, ex);
+         }
+
     }    
-    
+     static Connection conOracle(String id, String pw) throws Exception {
+        String connectionString = "jdbc:oracle:thin:@localhost:1521:XE";
+        OracleDataSource ds = new OracleDataSource();   
+        ds.setURL(connectionString);
+        return ds.getConnection(id, pw);
+    }
 }
