98aab083021c242ae72fbee5f01974f1e9be8d32
maxknight50 <maxknight50@gmail.com>
2022-06-03 13:11:45 -0400

    Supplier

--- NetBeans_HW1/src/grocery/Supplier.java
+++ NetBeans_HW1/src/grocery/Supplier.java
@@ -9,50 +9,68 @@ package grocery;
  * @author Jd3ab
  */
 public class Supplier {
-   private String name; 
-   private String address; 
-   private String contactName; 
-   private String contactPhone; 
-   private String contactEmail; 
-   
-   public String getName()
-   {
-     return this.name;
-   }
-   public void setName(String name)
-   {
-       this.name = name; 
-   }
-     public String getAddress()
-   {
-     return this.address;
-   }
-   public void setAdress(String address)
-   {
-       this.address = address; 
-   }
-   public String getContactName()
-   {
-       return this.contactName; 
-   }
-   public void setContactName(String contactName)
-   {
-       this.contactName = contactName; 
-   }
-   public String getContactPhone()
-   {
-       return this.contactPhone; 
-   }
-   public void setContactPhone(String contactPhone)
-   {
-       this.contactPhone = contactPhone; 
-   }
-   public String getContactEmail()
-   {
-       return this.contactEmail; 
-   }
-   public void setContactEmail(String contactEmail)
-   {
-       this.contactEmail = contactEmail; 
-   }
+
+    private String id;
+    private String name;
+    private String address;
+    private String contactName;
+    private String contactPhone;
+    private String contactEmail;
+
+    public Supplier(String id, String name, String address, String contactName, String contactPhone, String contactEmail) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.contactName = contactName;
+        this.contactPhone = contactPhone;
+        this.contactEmail = contactEmail;
+    }
+
+    public String getID() {
+        return this.id;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAdress(String address) {
+        this.address = address;
+    }
+
+    public String getContactName() {
+        return this.contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactPhone() {
+        return this.contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getContactEmail() {
+        return this.contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
 }
--- NetBeans_HW1/src/grocery/resources/SupplierController.java
+++ NetBeans_HW1/src/grocery/resources/SupplierController.java
@@ -1,6 +1,6 @@
 package grocery.resources;
 
-import grocery.Product;
+import grocery.Supplier;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -28,21 +28,21 @@ public class SupplierController implements Initializable {
      * Initializes the controller class.
      */
     @FXML
-    private TableView<Product> supplier_table; 
+    private TableView<Supplier> supplier_table; 
     @FXML
-    private TableColumn<Product, String> col_supp_id; 
+    private TableColumn<Supplier, String> col_supp_id; 
     @FXML 
-    private TableColumn<Product, String> col_supp_name; 
+    private TableColumn<Supplier, String> col_supp_name; 
     @FXML 
-    private TableColumn<Product, String> col_supp_address; 
+    private TableColumn<Supplier, String> col_supp_address; 
     @FXML 
-    private TableColumn<Product, String> col_con_name; 
+    private TableColumn<Supplier, String> col_con_name; 
     @FXML 
-    private TableColumn<Product, String> col_con_phone; 
+    private TableColumn<Supplier, String> col_con_phone; 
     @FXML 
-    private TableColumn<Product, String> col_con_email;
+    private TableColumn<Supplier, String> col_con_email;
     
-    ObservableList<Product> slist = FXCollections.observableArrayList();
+    ObservableList<Supplier> slist = FXCollections.observableArrayList();
     Connection conn; 
     Statement stmt; 
     
@@ -53,29 +53,35 @@ public class SupplierController implements Initializable {
          conn = oracle.getConnection();
          stmt = oracle.getStatement();
          ResultSet rs;
-         String queryString = "select * from product where product.product_id in " +
-            "(select inventory.product_id from inventory where inventory.area_id in " +
-            "(select storeArea.area_id from storeArea where storeArea.store_id in " +
-            "(select store_id from store where store.city = '" + Store.currentStore + "')))";
+         String queryString = "select * from supplier where supplier.supp_id in " +
+                "(select product.supp_id from product where product.area_id in" +
+                "(select storeArea.area_id from storeArea where storeArea.store_id in" +
+                "(select store_id from store where store.city = '" + Store.currentStore + "')))";
          System.out.println(queryString);
          
         try {
             rs = stmt.executeQuery(queryString);
             
             while (rs.next()){
-                slist.add(new Product(rs.getString("PRODUCT_ID"), rs.getString("PRODUCT_NAME"), rs.getDouble("PRICE_PAID"), rs.getString("PRODUCT_DESC"), rs.getString("SUPP_ID"), rs.getString("AREA_ID")));
+                slist.add(new Supplier(
+                        rs.getString("Supp_ID"), 
+                        rs.getString("SUPP_NAME"), 
+                        rs.getString("SUPP_ADDRESS"), 
+                        rs.getString("Sales_Contact_Name"), 
+                        rs.getString("Sales_Contact_Phone"), 
+                        rs.getString("Sales_Contact_Email")));
             }
             
         } catch (SQLException ex) {
-            Logger.getLogger(ProductsController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(SupplierController.class.getName()).log(Level.SEVERE, null, ex);
         }
          
-        col_supp_id.setCellValueFactory(new PropertyValueFactory<>("description"));
-        col_supp_name.setCellValueFactory(new PropertyValueFactory<>("productID"));
-        col_supp_address.setCellValueFactory(new PropertyValueFactory<>("price"));
-        col_con_name.setCellValueFactory(new PropertyValueFactory<>("name"));
-        col_con_phone.setCellValueFactory(new PropertyValueFactory<>("supID"));
-        col_con_email.setCellValueFactory(new PropertyValueFactory<>("areaID"));
+        col_supp_id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        col_supp_name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        col_supp_address.setCellValueFactory(new PropertyValueFactory<>("address"));
+        col_con_name.setCellValueFactory(new PropertyValueFactory<>("contactName"));
+        col_con_phone.setCellValueFactory(new PropertyValueFactory<>("contactPhone"));
+        col_con_email.setCellValueFactory(new PropertyValueFactory<>("contactEmail"));
         supplier_table.setItems(slist);
     }
 }
